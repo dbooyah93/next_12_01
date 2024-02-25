d02185c68c509d7a0ffe562667d30dbbc36c85c3
dbooyah93 <davidmcruz1125@gmail.com>
2024-02-25 15:30:05 -0600

    Custom 404 at the home page level

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function () {
+    return (
+        <>
+            <Link href="/">
+                <a>
+                    Home
+                </a>
+            </Link>
+            <h1> Try again </h1>
+        </>
+
+    )
+}
