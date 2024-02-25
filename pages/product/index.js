1c18be2f4953f4a5ebbb9c9414716e98f672eca2
dbooyah93 <davidmcruz1125@gmail.com>
2024-02-25 15:06:23 -0600

    implement for loop within component function

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,8 +1,23 @@
 import Link from "next/link";
 
 let product = function ({ productID = 100} ) {
+    let links = []
+    for ( let i = 0; i <= productID; i++ ) {
+        links.push(
+            <>
+                <Link href={`product/${i}`}>
+                    <a>
+                        Product: {i}
+                    </a>
+                </Link>
+                <br/>
+            </>
+        );
+    }
+
     return (
         <>
+            {links}
             <Link href='/'>
                 Home
             </Link>
@@ -12,7 +27,7 @@ let product = function ({ productID = 100} ) {
                 </Link>
             </p>
             <p>
-                <Link href="/product/2">
+                <Link href="/product/2" replace>
                     Product 2
                 </Link>
             </p>
